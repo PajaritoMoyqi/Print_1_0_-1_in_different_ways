6ef9ca2e7c8a42fd71fbeeb381246e1c39344675
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2024-07-29 11:15:48 +0900

    C - ASCII code [DONE]

---
+++ C/ASCII.c
@@ -0,0 +1,18 @@
+/*
+    This code prints out 0 and 1 using 'ASCII code'.
+    ASCII code is 7-bit code which represents printable text or control character.
+    If we print a ASCII character as digit("%d" in printf function), we can check its 7-bit number.
+
+    You can see a list of all ASCII characters at 'ASCII.md' in same folder.
+*/
+
+#include <stdio.h>
+
+int main( void )
+{
+    printf( "%d", NULL ); // NULL character, print 0
+    printf( "\n" );
+    printf( "%d", '' ); // SOH(Start Of Header), print 1
+
+	return 0;
+}
